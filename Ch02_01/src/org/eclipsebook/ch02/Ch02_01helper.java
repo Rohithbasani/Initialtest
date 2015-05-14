bfe7a8442e3b0fe174eea03eee45da1a28d2cd41
Rohith <Rohith@10.53.215.128>
2015-05-14 13:00:05 +0200

    Added simple sysout

--- Ch02_01/src/org/eclipsebook/ch02/Ch02_01helper.java
+++ Ch02_01/src/org/eclipsebook/ch02/Ch02_01helper.java
@@ -4,5 +4,7 @@ public class Ch02_01helper {
 	public void printer(){
 		System.out.println("No worries.");
 		System.out.println("Changes made to reflect in Github online");
+
+		System.out.println("Test1");
 	}
 }
